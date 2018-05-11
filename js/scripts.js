4b530fc1f4cc157f1a0854fecf61661f1cf50b34
Owen Muriithi <owenmuriithi@gmail.com>
2018-05-11 14:40:49 +0300

    Adds function for Player and its objects

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,10 @@
 var rolling = function(){
   return Math.floor(Math.random() * 6) + 1;
 }
+function Player(roll,tempscore,totalscore,turn,playername){
+  this.roll=0;
+  this.tempscore=0;
+  this.totalscore=0;
+  this.turn=0;
+  this.playername;
+}
